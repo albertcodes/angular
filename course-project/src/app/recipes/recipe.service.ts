9e46c5eeb92e106da33a8ac426465735a269437c
albertcodes <albertnthiwa@gmail.com>
2020-02-27 17:57:28 +0300

    added recipe to service

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -8,20 +8,30 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('Shnitzel Recipe',
+        new Recipe(
+            'Shnitzel Recipe',
             'Tasty shnitzel',
             'https://4.imimg.com/data4/YY/YY/GLADMIN-/assets-veg-20cutlet_small-500x500.png',
             [
                 new Ingredient('Meat', 1),
                 new Ingredient('French Fries', 20)
             ]),
-        new Recipe('Burger Recipe',
+        new Recipe(
+            'Burger Recipe',
             'Best burger ever!',
             'https://upload.wikimedia.org/wikipedia/commons/1/11/Cheeseburger.png',
             [
                 new Ingredient('Buns', 2),
                 new Ingredient('Steak', 1)
-            ])
+            ]),
+        new Recipe(
+            'Pizza Recipe',
+            'Delicious pepperoni pizza',
+            'https://upload.wikimedia.org/wikipedia/commons/e/e1/Pepperoni_pizza_%282%29.png',
+            [
+                new Ingredient('Flour', 1),
+                new Ingredient('Pepperoni', 20)
+            ]),
     ];
 
     constructor(private slService: ShoppingListService) {
