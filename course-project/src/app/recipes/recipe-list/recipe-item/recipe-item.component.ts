1b430e1518673fc1728458c69e5f90acb1da9a62
albertcodes <albertnthiwa@gmail.com>
2020-03-03 11:23:17 +0300

    passed dynamic parameters to links

--- course-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,7 +8,7 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
+  @Input() index: number;
 
   constructor() { }
 
