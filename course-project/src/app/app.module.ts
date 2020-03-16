f9a8356e51db11be3e8b5b3daa96afd43787b6b6
albertcodes <albertnthiwa@gmail.com>
2020-03-16 21:28:34 +0300

    restructuring services to use child injector

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -3,18 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
 
 // declarations:
 import { AppComponent } from './app.component';
-
-// providers:
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { RecipeService } from './recipes/recipe.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
-import { AuthModule } from './auth/auth.module';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -27,13 +20,7 @@ import { CoreModule } from './core/core.module';
     AuthModule,
     CoreModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    AuthGuard
-  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- course-project/src/app/core/core.module.ts
+++ course-project/src/app/core/core.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 
+// declarations & imports/exports:
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from '../app-routing.module';
+
+// providers:
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { AuthGuard } from '../auth/auth-guard.service';
 
 @NgModule({
     declarations: [
@@ -15,6 +23,13 @@ import { AppRoutingModule } from '../app-routing.module';
     exports: [
         AppRoutingModule,
         HeaderComponent
-    ]
+    ],
+    providers: [
+        ShoppingListService,
+        RecipeService,
+        DataStorageService,
+        AuthService,
+        AuthGuard
+      ],
 })
 export class CoreModule {}
