39d10ad3778cc2f9e88742306adebb97f8ed4f3b
albertcodes <albertnthiwa@gmail.com>
2020-03-20 18:31:18 +0300

    Adding actions

--- course-project/src/app/shopping-list/ngrx-store/shopping-list.reducers.ts
+++ course-project/src/app/shopping-list/ngrx-store/shopping-list.reducers.ts
@@ -1,8 +1,6 @@
 import { Action } from '@ngrx/store';
 
-import { Ingredient } from '../shared/ingredient.model';
-
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+import { Ingredient } from '../../shared/ingredient.model';
 
 const initialState = {
     ingredients: [
