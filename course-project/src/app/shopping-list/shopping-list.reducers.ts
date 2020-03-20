d4011d3b576cfc64d9699100f968c2c6f0f87d79
albertcodes <albertnthiwa@gmail.com>
2020-03-20 17:37:08 +0300

    Getting started with reducers

---
+++ course-project/src/app/shopping-list/shopping-list.reducers.ts
@@ -0,0 +1,23 @@
+import { Action } from '@ngrx/store';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+
+const initialState = {
+    ingredients: [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ]
+};
+
+export function ShoppingListReducer(state = initialState, action: Action) {
+    switch (action.type) {
+        case ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: [...state.ingredients]
+            };
+    }
+    return state;
+}
