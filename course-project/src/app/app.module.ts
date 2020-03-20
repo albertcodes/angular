6adffe720dd626e54ea378120e87109dfb616bb7
albertcodes <albertnthiwa@gmail.com>
2020-03-20 18:45:50 +0300

    registered the application state

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,9 +2,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import { ShoppingListReducer } from './shopping-list/ngrx-store/shopping-list.reducers';
 
 // declarations:
 import { AppComponent } from './app.component';
@@ -17,6 +20,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot({shoppingList: ShoppingListReducer}),
     AuthModule,
     CoreModule
   ],
