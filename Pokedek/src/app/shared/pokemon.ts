2bbdb8e332f7ba3c0bae693e6997d0e9d2f355cb
albertcodes <albertnthiwa@gmail.com>
2020-04-03 20:15:25 +0300

    organized project setup

--- Pokedek/src/app/shared/pokemon.ts
+++
@@ -1,8 +0,0 @@
-export interface Pokemon {
-  id: number;
-  name: string;
-  image: string;
-  species: string;
-  type: string[];
-  abilities: string[];
-}
--- Pokedek/src/app/shared/rxjs-extensions.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-require("rxjs/add/operator/do");
-require("rxjs/add/operator/catch");
-require("rxjs/add/operator/map");
-require("rxjs/add/observable/throw");
-//# sourceMappingURL=rxjs-extensions.js.map
