fc1a5327bdbde733ac284af57bbc0065a8a76f38
albertcodes <albertnthiwa@gmail.com>
2020-05-06 22:52:09 +0300

    shows caP

--- projects/tv-watcher/src/app/app.module.ts
+++ projects/tv-watcher/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PlayComponent } from './play/play.component';
+import { ShowsComponent } from './shows/shows.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PlayComponent
+    PlayComponent,
+    ShowsComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/tv-watcher/src/app/shows/shows.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shows',
+  templateUrl: './shows.component.html',
+  styleUrls: ['./shows.component.scss']
+})
+export class ShowsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
