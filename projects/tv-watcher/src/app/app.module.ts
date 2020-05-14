4650bb4da933aed322bea09acdeefec0bab6ebde
albertcodes <albertnthiwa@gmail.com>
2020-05-14 21:04:04 +0300

    delete mode: movies

--- projects/tv-watcher/src/app/app.module.ts
+++ projects/tv-watcher/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MoviesComponent } from './movies/movies.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { SeriesComponent } from './pages/series/series.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
 @NgModule({
   declarations: [
